f306076345204935d783d0aa5770c04f94a28219
XenonLab <xenonlab.develop@gmail.com>
2019-03-29 17:41:10 +0100

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    print("{0} pressed".format(key))
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press = on_press, on_release = on_release) as listener:
+    listener.join()
